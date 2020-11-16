fe3b850bc28129fd8856966d88cdbc33e06c71fa
Vitaly <71028477+MrNegativ@users.noreply.github.com>
2020-11-16 14:24:45 +0300

    Add files via upload

---
+++ ShowFilesDemo/ShowFilesDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShowFilesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
